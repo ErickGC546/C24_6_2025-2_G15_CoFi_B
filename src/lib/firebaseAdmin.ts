aa5f35bcd3b1fa9bc60a76cc98924c186ba260fb
ErickGC546 <erick.galindo.c@tecsup.edu.pe>
2025-12-10 02:06:48 -0500

    commit firebaseses

--- src/lib/firebaseAdmin.ts
+++ src/lib/firebaseAdmin.ts
@@ -1,16 +1,10 @@
 import * as admin from "firebase-admin";
 
-if (!process.env.FIREBASE_SERVICE_ACCOUNT) {
-  throw new Error("FIREBASE_SERVICE_ACCOUNT env variable is not set");
-}
-
-// Convierte los saltos de línea escapados en saltos de línea reales
-const serviceAccountString = process.env.FIREBASE_SERVICE_ACCOUNT.replace(/\\n/g, '\n');
-const serviceAccount = JSON.parse(serviceAccountString);
-
 if (!admin.apps.length) {
   admin.initializeApp({
-    credential: admin.credential.cert(serviceAccount),
+    credential: admin.credential.cert(
+      JSON.parse(process.env.FIREBASE_SERVICE_ACCOUNT!)
+    ),
   });
 }
 
